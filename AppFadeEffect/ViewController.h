1a71e8ad44c012300df804bcbab577f135e24c25
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-02-19 21:24:09 -0600

    App fade effect

--- AppFadeEffect/ViewController.h
+++ AppFadeEffect/ViewController.h
@@ -8,6 +8,14 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segControl;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+
+- (IBAction)fadeIn:(id)sender; //aparecer
+- (IBAction)fadeOut:(id)sender; //desaparecer
+- (IBAction)amIFaded:(id)sender;
 
 
 @end
--- AppFadeEffect/ViewController.m
+++ AppFadeEffect/ViewController.m
@@ -18,5 +18,28 @@
     // Do any additional setup after loading the view.
 }
 
+//Para hacer que los controles aparezcan
+- (IBAction)fadeOut:(id)sender {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:5.0];
+    [self.slider setAlpha:1.0];
+    [self.segControl setAlpha:1.0];
+    [UIView commitAnimations];
+}
 
+//Para hacer que los controles desaparezcan
+- (IBAction)fadeIn:(id)sender {
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:5.0];
+    [self.slider setAlpha:0.0];
+    [self.segControl setAlpha:0.0];
+    [UIView commitAnimations];
+}
+- (IBAction)amIFaded:(id)sender {
+    if (self.slider.alpha == 1.0) {
+        self.label.text = @"The objects are visible";
+    }else{
+        self.label.text = @"The objects are invisible";
+    }
+}
 @end
